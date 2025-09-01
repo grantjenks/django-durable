f5d68bd7c8e2097203157c05f4e9c4a46f954ee7
Grant Jenks <grant.jenks@gmail.com>
2025-09-01 11:44:50 -0700

    improve sidebar formatting

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,28 @@ exclude_patterns: list[str] = []
 html_theme = 'alabaster'
 html_static_path = ['_static']
 html_logo = '_static/django-durable-logo.svg'
+html_css_files = ['custom.css']
+
+# Sidebar layout: put search at the bottom
+html_sidebars = {
+    '**': [
+        'about.html',          # shows logo, project name, description
+        'navigation.html',
+        'relations.html',
+        'searchbox.html',
+    ]
+}
+
+# Alabaster theme options: center name, show GitHub star button
+html_theme_options = {
+    'logo_name': True,
+    'logo_text_align': 'center',
+    'github_user': 'grantjenks',
+    'github_repo': 'django-durable',
+    'github_button': True,       # show GitHub star button
+    'github_type': 'star',
+    'github_count': True,
+}
 
 # Autodoc defaults
 autodoc_default_options = {
